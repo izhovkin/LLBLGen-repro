14f8a8827a85d0f0ddf35b9f99d262116e1c2457
Ilya Izhovkin <izhovkin@gmail.com>
2026-03-19 11:13:39 +0100

    Added reproduction example

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using NWService.Dal.EntityClasses;
+
+namespace TestProject1;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var order = new OrderEntity()
+        {
+            ActiveContext = new SD.LLBLGen.Pro.ORMSupportClasses.Context()
+        };
+
+        var detail = new OrderDetailEntity();
+
+        order.OrderDetails.Add(detail);
+        order.OrderDetails.Add(detail);
+
+        Assert.That(order.OrderDetails.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var order = new OrderEntity()
+        {
+            ActiveContext = new SD.LLBLGen.Pro.ORMSupportClasses.Context()
+        };
+
+        var detail = new OrderDetailEntity()
+        {
+            Order = order
+        };
+
+        order.OrderDetails.Add(detail);
+
+        Assert.That(order.OrderDetails.Count, Is.EqualTo(1));
+    }
+}
